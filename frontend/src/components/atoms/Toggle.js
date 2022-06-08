9d1b2dcec26ed068c40cf90a51d658bdd1351903
Naren <narensreekanth@gmail.com>
2022-06-08 17:12:26 +0530

    initial homepage design

---
+++ frontend/src/components/atoms/Toggle.js
@@ -0,0 +1,116 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  View,
+  Text,
+  Animated,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions,
+} from 'react-native';
+import {BORDER_RADIUS} from '../../styles/mixins';
+
+const Toggle = ({
+  onSignUpPress,
+  onSignInPress,
+  initialState,
+  unselectionColor,
+  selectionColor,
+  leftContent,
+  rightContent,
+  height,
+  width
+}) => {
+  const [active, setActive] = useState(initialState);
+  let transformX = useRef(new Animated.Value(0)).current;
+  const WIDTH = height;
+  const HEIGHT = width;
+
+  useEffect(() => {
+    if (active) {
+      Animated.timing(transformX, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(transformX, {
+        toValue: 0,
+        duration: 100,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [active]);
+
+  const rotationX = transformX.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, WIDTH / 2],
+  });
+
+  return (
+    <SafeAreaView
+      style={{
+        width: WIDTH,
+        alignItems: 'center',
+      }}>
+      <View
+        style={{
+          left: Dimensions.get('screen').width / 2 - WIDTH / 2,
+          flexDirection: 'row',
+          position: 'relative',
+          height: HEIGHT,
+          width: WIDTH,
+          borderRadius: BORDER_RADIUS,
+          backgroundColor: unselectionColor,
+        }}>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            height: HEIGHT,
+            // top: 2,
+            // bottom: 2,
+            borderRadius: BORDER_RADIUS,
+            width: WIDTH / 2,
+            transform: [
+              {
+                translateX: rotationX,
+              },
+            ],
+            backgroundColor: selectionColor,
+          }}></Animated.View>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => {
+            setActive(false);
+            if (active) {
+              onSignUpPress();
+            }
+          }}>
+          <Text style={{color: active ? 'black' : 'white', fontWeight: 'bold'}}>
+            {leftContent}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => {
+            setActive(true);
+            if (!active) {
+              onSignInPress();
+            }
+          }}>
+          <Text style={{color: active ? 'white' : 'black', fontWeight: 'bold'}}>
+           {rightContent}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default Toggle;
--- frontend/src/scenes/home/home.js
+++ frontend/src/scenes/home/home.js
@@ -1,49 +1,13 @@
 import * as React from 'react';
 import {StyleSheet, View, SafeAreaView} from 'react-native';
-import { Text } from 'react-native-elements';
-import {PRIMARY, SECONDARY} from '../../styles/colors';
-import Toggle from '../../components/atoms/Toggle';
-//import RestaurantCard from '../../components/atoms/RestaurantCard';
+import RestaurantScroll from '../../components/molecules/RestaurantScroll'
+import TopBarHome from '../../components/molecules/TopBarHome';
 
-// const HomeScreen = props => {
-//   return (
-//     <SafeAreaView style={{backgroundColor: BACKGROUND_COLOR, flex: 1}}>
-//       <TopBarHome
-//         selectionState={props.auth}
-//         onPress={() => props.navigation.navigate('Landing')}
-//         onSignInPress={() => props.toggleAuthState(true)}
-//         onSignUpPress={() => props.toggleAuthState(false)}
-//       />
-//     </SafeAreaView>
-//   );
-// };
-
-// const mapStateToProps = state => {
-//   const {auth} = state;
-//   return {auth};
-// };
-
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators(
-//     {
-//       toggleAuthState,
-//     },
-//     dispatch,
-//   );
 const HomeScreen = () => {
   return (
-    <View>
-      <Toggle
-        initialState={false}
-        onSignInPress={() => {}}
-        onSignUpPress={() => {}}
-        selectionColor={PRIMARY}
-        unselectionColor={SECONDARY}
-        leftContent = {"Available Restaurants"}
-        rightContent = {"Be a Transporter"} 
-        height = {350}
-        width = {50}
-      />
+    <View style={{backgroundColor: '#fafafa', flex: 1, }}>
+      <TopBarHome/>
+      <RestaurantScroll/>
       </View>
       
   );
